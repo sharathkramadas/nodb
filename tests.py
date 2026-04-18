5cea9fed96d40168620b41efef600f8e5ae670a9
sharathkramadas <sharathkramadas@gmail.com>
2026-04-18 11:40:08 +0530

    bug fixes

--- tests.py
+++ tests.py
@@ -1,21 +1,16 @@
 import os
 from repository.utils import GitUtils
 from gradle.utils import GradleUtils
+from maven.utils import MavenUtils
 from db.osv_lite import query
-
-def find_pom_files(root_dir):
-    pom_files = []
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        if "pom.xml" in filenames:
-            pom_files.append(dirpath)
-    return pom_files
 
 repo = "https://github.com/cdefense/vulnerable-java-maven"
 # repo = "https://github.com/WebGoat/WebGoat"
 repo = "https://github.com/veracode/verademo"
 repo = "https://github.com/melix/maven-repository-injection"
 # GitUtils().clone_public_repo(repo)  
-# repos = find_pom_files(GitUtils().repo_path)
+# maven = MavenUtils(project_dir="/tmp/repo")
+# maven.fix_vuls()
 
 repo = "https://github.com/DataDog/vulnerable-java-application.git"
 
